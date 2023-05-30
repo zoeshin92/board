f16b6ec9c0ad5ab0014fe4ccfc28a3801673015a
zoeshin <zoeshin92@gmail.com>
2023-05-30 17:39:44 +0900

    VO 생성

--- src/main/kotlin/com/ohlab/board/BoardApplication.kt
+++ src/main/kotlin/com/ohlab/board/BoardApplication.kt
@@ -1,9 +1,10 @@
 package com.ohlab.board
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [DataSourceAutoConfiguration::class])
 class BoardApplication
 
 fun main(args: Array<String>) {
---
+++ src/main/kotlin/com/ohlab/board/BoardVO.kt
@@ -0,0 +1,8 @@
+package com.ohlab.board
+
+data class BoardVO (
+    var id: Int,
+    var title: String,
+    var content: String,
+    var createDate: String
+)
